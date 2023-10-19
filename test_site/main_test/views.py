917a7d21970c9a2c54227b552b9a1cb4355f0405
GVladislav <grishinvladislav97@gmail.com>
2023-10-19 18:06:39 +0300

    Add redirect to home page from "/"

--- test_site/main_test/views.py
+++ test_site/main_test/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 
 def main_page(request):
     return render(request, 'main_test/main_test.html')
+
+
+def home(request):
+    return redirect('main_page', permanent=True)
