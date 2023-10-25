58299becbcc92f094a88751943d7de028c61bca8
GVladislav <grishinvladislav97@gmail.com>
2023-10-25 20:00:35 +0300

    Create quiz list, quiz page, quiz content endpoints

--- test_site/main_test/views.py
+++ test_site/main_test/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import Quiz
 
 
 def main_page(request):
-    return render(request, 'main_test/main_test.html')
+    context = {'quizzes': Quiz.published.all()}
+    return render(request, 'main_test/main_test.html', context)
 
 
 def home(request):
     return redirect('main_page', permanent=True)
+
+
+def show_quiz(request, quiz_slug):
+    quiz = get_object_or_404(Quiz, slug=quiz_slug)
+    context = {'quiz': quiz}
+    return render(request, 'main_test/quiz_page.html', context)
+
+
+def show_quiz_content(request, quiz_slug):
+    quiz = get_object_or_404(Quiz, slug=quiz_slug)
+    context = {'quiz': quiz}
+    return render(request, 'main_test/quiz_content.html', context)
