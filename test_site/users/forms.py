6fede7876224eb820f836f1d2a492329a69508e3
Tsvetkov <zwetkow7@gmail.com>
2023-10-31 17:22:57 +0300

    1. Changing in base.html. creating registration, authorization and authentication. In base.html added login, logout and registration. 2. Changing in settings.py. added default settings for unauthorized users (can't see internal conten). May be need upgrade... 3. Added forms.py in directory 'users'. Default-classes forms for creating login-form, register_users-form, including cheking password (double password entry during registration). Mandatory parameters in registration - username, email, password and password2. 4. In urls.py added 2 urls - register/ and register_done/ 5. In views.py added views for users registration.

--- test_site/users/forms.py
+++ test_site/users/forms.py
@@ -1,7 +1,35 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
 
 class LoginUserForm(forms.Form):
     username = forms.CharField(label="Логин",
-                    widget=forms.TextInput(attrs={"class": "forms-input"}))
+                               widget=forms.TextInput(attrs={"class": "forms-input"}))
     password = forms.CharField(label="Пароль",
-                    widget=forms.PasswordInput(attrs={"class": "forms-input"}))
+                               widget=forms.PasswordInput(attrs={"class": "forms-input"}))
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'password2']
+        labels = {
+            'email': 'E-mail',
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Пароли не совпадают!")
+        return cd['password2']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Такой E-mail уже существует!")
+        return email
